83a3f091dadd2bdda6370758486f27d024ed8123
stanbarosan <151366282+stanbarosan@users.noreply.github.com>
2024-05-04 21:16:09 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+
+bool buton;
+
+struct intrebare
+{
+    int nr;
+    char text[100];
+    bool raspuns;
+};
+
+
+int main()
+{
+
+}
